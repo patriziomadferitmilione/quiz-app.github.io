11caebe26658859153a7664104e5ba8c49f0ddc4
Patrizio Milione <84288560+patriziomadferitmilione@users.noreply.github.com>
2022-06-01 09:38:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,104 +1,33 @@
-const quizData = [
-    {
-        question: 'How tall is the Eiffel Tower?',
-        a: '473m',
-        b: '203m',
-        c: '324m',
-        d: '749m',
-        correct: 'c'
-    }, {
-        question: 'When was the printing press invented?',
-        a: '1267',
-        b: '1680',
-        c: '1456',
-        d: '1789',
-        correct: 'c'
-    }, {
-        question: 'When was Stg. Peppers released?',
-        a: '1980',
-        b: '1967',
-        c: '1975',
-        d: '1993',
-        correct: 'b'
-    }, {
-        question: 'How many sonnets did Shakespeare write?',
-        a: '168',
-        b: '30',
-        c: '518',
-        d: '1',
-        correct: 'a'
-    }, {
-        question: 'Which type of food are peanuts?',
-        a: 'Nuts',
-        b: 'Cereal',
-        c: 'Fruit',
-        d: 'Legume',
-        correct: 'd'
-    },
-];
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
 
-let currentQuiz = 0;
-let score = 0;
+const newYears = '1 Jan 2023';
 
-loadQuiz();
+function countdown() {
+    const newYearsDate = new Date(newYears)
+    const currentDate = new Date();
 
-function loadQuiz() {
-    deselectAnswers();
+    const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
 
-    const currentQuizData = quizData[currentQuiz];
-
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = (Math.floor(totalSeconds / 60) % 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+    
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
 }
 
-function getSelected() {
-    let answer = undefined;
-
-    answerEls.forEach((answerEl) => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
-
-    return answer;
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
-function deselectAnswers() {
-    answerEls.forEach((answerEl) => {
-       answerEl.checked = false;
-    });
-}
+// initial call
+countdown();
 
-submitBtn.addEventListener("click", () => {
-    // check to see the answer
-    const answer = getSelected();
-
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-
-        currentQuiz++;
-        if(currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-                
-                <button onclick="location.reload()">Reload</button>
-            `;
-        }    
-    }
-});
+setInterval(countdown, 1000);
